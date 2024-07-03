5ebf3d30aeb385205e5bd93c219eeddc700e9937
RokasMesk <rokasme@wix.com>
2024-07-03 11:18:53 +0300

    added semantic html fixed naming

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const calendarCells = document.getElementById('calendarCells');
-    const hoursInDay = 19;
-    const daysInWeek = 7;
-    for (let i = 0; i < hoursInDay * daysInWeek; i++) {
-        const cell = document.createElement('div');
-        cell.className = 'cell';
-        calendarCells.appendChild(cell);
-    }
-});
+function fillCells() {
+    document.addEventListener('DOMContentLoaded', () => {
+        const calendarCells = document.getElementById('calendarCells');
+        const cellsInOneColumn= 19;
+        const daysInWeek = 7;
+        for (let i = 0; i < cellsInOneColumn * daysInWeek; i++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            calendarCells.appendChild(cell);
+        }
+    });
+}
+
+function init() {
+    fillCells();
+}
+
+init();
