9a7757ad0fa5c81d9cc0bc98dbac0ae43bf06229
RokasMesk <rokasme@wix.com>
2024-07-07 17:36:23 +0300

    validation-startdate-endDate

--- js/modal.js
+++ js/modal.js
@@ -6,6 +6,20 @@ function closeEventCreationModal() {
     const modal = document.getElementById("eventModal");
     modal.style.display = "none";
 }
+function validateEventForm(event) {
+    const startDate = document.getElementById("startDate").value;
+    const endDate = document.getElementById("endDate").value;
+    const startTime = document.getElementById("startTime").value;
+    const endTime = document.getElementById("endTime").value;
+    const startDateTime = new Date(`${startDate}T${startTime}`);
+    const endDateTime = new Date(`${endDate}T${endTime}`);
+
+    if (startDateTime > endDateTime) {
+        event.preventDefault();
+        alert("End date and time must be after start date and time");
+    }
+
+}
 export function initModalEventListeners() {
     const createEventButton = document.querySelector(".add-event-button");
     const closeModalButton = document.getElementById("closeModal")
@@ -19,4 +33,6 @@ export function initModalEventListeners() {
             closeEventCreationModal();
         }
     });
+    const eventForm = document.getElementById("eventForm");
+    eventForm.addEventListener('submit', validateEventForm)
 }
