12b19b7347acc3252fd5cb15da4aee96a9dc090a
RokasMesk <rokasme@wix.com>
2024-07-18 10:44:10 +0300

    Remove dist folder from tracking

--- dist/types.js
+++
@@ -1,6 +0,0 @@
-export var ButtonAction;
-(function (ButtonAction) {
-    ButtonAction[ButtonAction["Today"] = 0] = "Today";
-    ButtonAction[ButtonAction["PreviousWeek"] = 1] = "PreviousWeek";
-    ButtonAction[ButtonAction["NextWeek"] = 2] = "NextWeek";
-})(ButtonAction || (ButtonAction = {}));
